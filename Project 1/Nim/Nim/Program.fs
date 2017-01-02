b75745f35bb6fc72830750c12356cdccf8c029d4
mhaubro <martin.haubro@hotmail.com>
2017-01-02 14:34:51 +0100

    Adding basic UI (More like how-to)

--- Project 1/Nim/Nim/Program.fs
+++ Project 1/Nim/Nim/Program.fs
@@ -5,6 +5,7 @@
 //Dette er nødvendigt for at få compiler til at køre.
 open System
 open System.Windows.Forms
+open System.Drawing
 open WindowStartScreen
 open WindowGameScreen
 
@@ -12,7 +13,7 @@ open WindowGameScreen
 [<EntryPoint>]
 [<STAThread>]
 let main argv = 
-    use form = new Form()
- 
-    Application.Run(form);
+    let window = new Form(Text="HI!", Size=Size(500, 500))
+    setUpForm(window)
+    Application.Run(window);
     0 // return an integer exit code
