3201f35f66a5d5a9b50a3af96c711550590117dd
mhaubro <martin.haubro@hotmail.com>
2017-01-02 13:10:16 +0100

    small things

--- Project 1/Nim/Nim/Program.fs
+++ Project 1/Nim/Nim/Program.fs
@@ -1,8 +1,13 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
+//Denne file skal være NEDERST i hierakiet i view. 
+//Dette er nødvendigt for at få compiler til at køre.
 open System
 open System.Windows.Forms
+open WindowStartScreen
+open WindowGameScreen
+
 
 [<EntryPoint>]
 [<STAThread>]
