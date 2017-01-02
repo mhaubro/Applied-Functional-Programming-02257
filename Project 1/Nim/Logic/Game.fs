ea1e639076f8dbd548bf3ed7cc46f4ba7be55aee
Mathias Gammelmark <mathiasgam@hotmail.com>
2017-01-02 13:47:44 +0100

    eventqueue
    added the eventqueue from the example. and placed it in the game logic
    file. renamed gamelogic file to "Game"

---
+++ Project 1/Nim/Logic/Game.fs
@@ -0,0 +1,22 @@
+﻿module Game
+
+// Prelude
+open System 
+open System.Net 
+open System.Threading
+
+type Heap = int
+type User = string
+type Opponent = string
+
+type Game = Heap list * User * Opponent
+
+// Events for the AsyncEventQueue
+type Message = 
+    | Start of Game | UserMove of Game | OpponentMove of Game | End | Error | Clear
+let ev = AsyncEventQueue()
+let rec ready() = async {}
+and userInput() = async {}
+and opponentInput() = async {}
+and gameEnded() = async {}
+
