0717e54c34475d14dd94a6343b6f16caf18b005b
KongKrarup <khkrarup@gmail.com>
2017-01-03 12:03:44 +0100

    AI array change

--- Project 1/Nim/Logic/AI.fs
+++ Project 1/Nim/Logic/AI.fs
@@ -1,16 +1,16 @@
 ﻿module AI
 
-let rec critListReplace crit rep l = match l with
-                                        | [] -> []
-                                        | v::l' when crit v -> rep v::l'
-                                        | v::l' -> v::critListReplace crit rep l';;
-
 let heapOp hl =
-    let m = List.fold (^^^) 0 hl
+    let m = Array.fold (^^^) 0 hl
+    let hl' = Array.copy hl
     match m with
-    | 0 -> let maxv = List.max hl
-           critListReplace (fun v -> v=maxv) (fun v -> v-1) hl
-    | _ -> critListReplace (fun v -> v ^^^ m < v) (fun v -> v ^^^ m) hl;;
+    | 0 -> let maxv = Array.max hl 
+           let maxi = Array.findIndex (fun v -> v=maxv) hl
+           hl'.[maxi] <- 0;
+    | _ -> let vi = Array.findIndex (fun v -> v ^^^ m < v) hl
+           hl'.[vi] <- hl.[vi]^^^m
+    |> ignore
+    hl'
            
 let opponentAI g = match g with
                     | (hl, o, p) -> (heapOp hl, o, p)
