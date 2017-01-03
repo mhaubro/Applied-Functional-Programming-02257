eb2b75c204849abb2f755a48fd4eb14a55c4945a
mhaubro <martin.haubro@hotmail.com>
2017-01-03 12:36:22 +0100

    work work

--- Project 1/Nim/Nim/WindowGameScreen.fs
+++ Project 1/Nim/Nim/WindowGameScreen.fs
@@ -3,25 +3,60 @@ open System.Windows.Forms
 open Game
 //This module describes the screen that's displayed when the user is playing.
 //First a simple description of how heaps look is developed.
-//type Game = Heap list * User * Opponent
+//type Game = Heap [] * User * Opponent
 
 //Combobox containing list for drop-down menu with heaps
 let ComboboxHeaps = new ComboBox()
-//Combobox for dropdown menu with matches.
-let ComboboxMatches = new ComboBox()
+//numericUpDown for matches.
+let numericUpDown = new NumericUpDown()
+
+//NumericUPDown
+let setUpMatchNumericUpDown max = //NumericUpDown is based on decimals
+    numericUpDown.Value <- (decimal) 1
+    numericUpDown.Increment <- (decimal) 1
+    numericUpDown.Minimum <- (decimal) 1
+    numericUpDown.Maximum <- (decimal) max
+
+let addIfNotZero heap index =
+    if (heap > 0) then ComboboxHeaps.Items.Add(index)|>ignore
 
 //Genererating heaps
-let loadHeaps (heaplist:List<int>) =
-    ComboboxHeaps.Items.Clear() 
-    ComboboxHeaps.Items.Add(heaplist)
+let loadHeaps (heaparray) =
+    ComboboxHeaps.Items.Clear()
+    Array.iteri(fun i heap -> addIfNotZero heap i) heaparray
 
-//Gives the amount of matches from the heap selected in the first comboboxheaps.
-//let numberMatches =
+
+let setUpGameScreen (game:Game) = 
+    loadHeaps (fst game)
+    setUpMatchNumericUpDown ComboboxHeaps.SelectedValue
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Gets the nth element of a list.
+//let rec getNth list n = 
+//    match n with
+//    | head::tail N when N = 0 -> head
+//    | head::tail N when N > 0 -> getNth(tail, N)
+//    | _ -> failwith("Error")
+
 
 //Finds the amount of matches to be selected, such that one can remove from the heap.
-let loadMatchRemoval = 
-    ComboboxMatches.Items.Clear()
-//    ComboboxMatches.Items.Add(Seq.toList(seq {1 ..  numberMatches}))
     
 
 //heapVisuals contains the elements to be shown.
