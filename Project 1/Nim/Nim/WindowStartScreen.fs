626e40aa1b046ca29f3076d2adf4bd7bbf47286f
mhaubro <martin.haubro@hotmail.com>
2017-01-04 14:55:57 +0100

    ed

--- Project 1/Nim/Nim/WindowStartScreen.fs
+++ Project 1/Nim/Nim/WindowStartScreen.fs
@@ -4,6 +4,15 @@ open System.Windows.Forms
 open System.Drawing
 open WindowGameScreen
 
+type StartScreen (form:System.Windows.Forms.Form) =
+    let _rand = new Button(Location=Point(50,65),MinimumSize=Size(100,50),
+                 MaximumSize=Size(100,50),Text="Completely Random", Visible=false)    
+
+    let _semirand = new Button(Location=Point(200,65),MinimumSize=Size(100,50),
+              MaximumSize=Size(100,50),Text="User picks heap amount", Visible=false)
+
+    let _custom = new Button(Location=Point(350,65),MinimumSize=Size(100,50),
+              MaximumSize=Size(100,50),Text="Completely Userdefined", Visible=false)
 //Button for starting a game with a randomly defined number of heaps with
 //Randomly defined matches (1-6 heaps with 1-6 matches)
 let startButton =
@@ -19,8 +28,6 @@ let startUserDefinedHeapButton =
 //Button for starting a game with number of heaps and number of matches
 //In each heap user defined
 let startUserDefinedAllButton =
-  new Button(Location=Point(350,65),MinimumSize=Size(100,50),
-              MaximumSize=Size(100,50),Text="Completely Userdefined", Visible=false)
 
 //Adds the controls to the basic form. Everything is initialized as being turned off
 let initializeStart (form:System.Windows.Forms.Form) =
