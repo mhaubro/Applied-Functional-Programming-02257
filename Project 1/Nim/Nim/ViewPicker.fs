ce2e4c75537faf738b2b97a5614a0c063e970aca
mhaubro <martin.haubro@hotmail.com>
2017-01-05 13:19:32 +0100

    Small changes

--- Project 1/Nim/Nim/ViewPicker.fs
+++ Project 1/Nim/Nim/ViewPicker.fs
@@ -3,31 +3,10 @@ open WindowGameScreen
 open WindowStartScreen
 open Game
 open GuiCallInterface
-//This is the ultimate picker of views.
 
-//let initializeForm (form:System.Windows.Forms.Form) =
-    //initializeGame form
-    //initializeStart form
-    //Picks the form to start with
-    //showStartScreen form
 
-    //The following two methods are used when a click happens, and the functions
-    //Are called.
-
-//let gotoGame gui = 
-//    showGameScreen gui
-//    hideStartScreen gui
-
-//let gotoStart (gui:GUI) =
-//    showStartScreen gui 
-//    hideGameScreen gui
-
-    //Must be called to fix the functions to buttons.
 let initguiButtons (gui:GUI) (SC:StartScreen) =
-    //As of right now, these do the same.
-//    startButton.Click.Add(fun _ -> Game.startGameFromUI {heaps, {Name="User"; getMove=gui.getUserMove}})
-    //startUserDefinedHeapButton.Click.Add(gotoGame)
-    //startUserDefinedAllButton.Click.Add(gotoGame)
+
     gui.backButton.Click.Add(fun  evArgs -> hideGameScreen gui 
                                             showStartScreen SC)
     gui.okButton.Click.Add(fun _ -> gui.eventQueue.Post (getSelected gui))
