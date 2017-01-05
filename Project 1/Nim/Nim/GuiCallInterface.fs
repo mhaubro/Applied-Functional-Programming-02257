1a450d488eb050daa249a73085252781eb7466d7
mhaubro <martin.haubro@hotmail.com>
2017-01-05 10:20:03 +0100

    Bugfixing

--- Project 1/Nim/Nim/GuiCallInterface.fs
+++ Project 1/Nim/Nim/GuiCallInterface.fs
@@ -1,22 +1,22 @@
 ﻿module GuiCallInterface
 open Game
+open WindowGameScreen
 
-let getUserMove heaparray gui =
-    Move(1,1)
-//    let game = (heaparray, "", "")
-//        use ts = new CancellationTokenSource()
-//        Async.StartWithContinuations
-//            (async {return PlayerB.getMove heapArray},
-//            (fun (id,num)   -> ev.Post (Move(id,num))),
-//            (fun _          -> ev.Post Error),
-//            (fun _          -> ev.Post Cancelled),
-//            ts.Token)
-    
-    
-    
-    
-//    ()
-    
+//Might work
+let getUserMove heaparray (gui:GUI) =
+    //Shows the gui
+    showGameScreen gui
+    setUpGameScreen gui
+    //Listens for gui input
+    let mutable move = (0,0)
+    Async.StartImmediate(async{
+        //failwith("Receive")
 
-
-
+        let! msg = gui.eventQueue.Receive()
+        
+        match msg with
+        | (a, b) ->    hideGameScreen gui//Hides the gui again 
+                       do move <- (a, b)//Lazy fix
+        | _ ->  hideGameScreen gui//Hides the gui again
+                return failwith("Unexpected user input")})
+    move
