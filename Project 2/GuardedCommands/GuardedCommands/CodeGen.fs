866bfd871abe84c57d6abb99ec85c32d5f4b3632
mhaubro <martin.haubro@hotmail.com>
2017-01-11 00:52:19 +0100

    A0-A3 running

--- Project 2/GuardedCommands/GuardedCommands/CodeGen.fs
+++ Project 2/GuardedCommands/GuardedCommands/CodeGen.fs
@@ -38,12 +38,18 @@ module CodeGeneration =
                                 CE vEnv fEnv b1 @ [IFZERO labfalse] @ CE vEnv fEnv b2
                                 @ [GOTO labend; Label labfalse; CSTI 0; Label labend]
 
-       | Apply(o,[e1;e2]) when List.exists (fun x -> o=x) ["-";"+"; "*"; "="]
+       | Apply(o,[e1;e2]) when List.exists (fun x -> o=x) ["-";"+"; "*"; "=";"<";"!";">";"<=";"<>"]
                              -> let ins = match o with
                                           | "-"  -> [SUB]
                                           | "+"  -> [ADD]
                                           | "*"  -> [MUL]
                                           | "="  -> [EQ] 
+                                          | "<"  -> [LT]
+                                          | "!"  -> [NOT]
+                                          | ">"  -> [SWAP; LT; NOT] 
+                                          | "<="  -> [SWAP; LT; NOT]
+                                          | ">="  -> [LT; NOT]
+                                          | "<>"  -> [EQ; NOT] 
                                           | _    -> failwith "CE: this case is not possible"
                                 CE vEnv fEnv e1 @ CE vEnv fEnv e2 @ ins
        | Apply(f,es)        -> match Map.tryFind f fEnv with
--- Project 2/GuardedCommands/GuardedCommands/Script.fsx
+++ Project 2/GuardedCommands/GuardedCommands/Script.fsx
@@ -89,7 +89,7 @@ List.iter exec ["Ex7.gc"; "fact.gc"; "factRec.gc"; "factCBV.gc"];;
 ((goTrace (parseFromFile "A0.gc")))
 
 // Test of programs covered by the fourth task (Section 5.4):
-List.iter exec ["A0.gc"; (*"A1.gc"; "A2.gc"; "A3.gc"*)];;
+List.iter exec ["A0.gc"; "A1.gc"; "A2.gc"; "A3.gc"];;
 
 // Test of programs covered by the fifth task (Section 6.1):
 //List.iter exec ["A4.gc"; "Swap.gc"; "QuickSortV1.gc"];;
