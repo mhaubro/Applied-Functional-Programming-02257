a2c5598bdc2beb5199e83ad1d1e2f0154ca75a45
mhaubro <martin.haubro@hotmail.com>
2017-01-13 11:04:12 +0100

    Switching to other branch

--- Project 2/GuardedCommands/FunctionalPearls/Trees.fs
+++ Project 2/GuardedCommands/FunctionalPearls/Trees.fs
@@ -1,5 +1,7 @@
 ﻿module Trees
     type 'a tree = Node of 'a * 'a tree list
 
+    let movetree = function
+                   | (Node((label, x), subtrees), x') -> Node((label, x+x'), subtrees)
     
     
