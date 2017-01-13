ce8f56eb3721ae42ebf13e75b33bd46661815cdc
mhaubro <martin.haubro@hotmail.com>
2017-01-13 12:01:50 +0100

    FSharpTranslatedFromSML

--- Project 2/GuardedCommands/FunctionalPearls/Extents.fs
+++ Project 2/GuardedCommands/FunctionalPearls/Extents.fs
@@ -12,19 +12,19 @@
     let rmax a b =if a>b then a else b
 
     let rec fit a b = match a, b with
-                             | (p,_)::ps, (_,q)::qs -> rmax (fit ps qs) (p-q+1.0)
+                             | (_,p)::ps, (q,_)::qs -> rmax (fit ps qs) (p-q+1.0)
                              | _, _ -> 0.0
 
     let rec fitll acc l = match acc,l with 
                              | _, [] ->[]
-                             |acc', e::es -> let v = fit acc [e] in
-                                                 v::(fitll (merge acc [moveextent(e,v)] ) es)
-    let fitlistl list = fitll [] list
+                             |acc', e::es -> let v = fit acc (e) in
+                                                 v::(fitll (merge (acc) (moveextent(e,v)) ) es)
+    let fitlistl list = List.rev (fitll ([]) (List.rev list))
     
     let rec fitlr acc l = match acc,l with 
                              | _, [] ->[]
-                             |acc', e::es -> let v = fit [e] acc in
-                                                 v::(fitlr (merge acc [moveextent(e,v)] ) es)
+                             |acc', e::es -> let v = fit (e) acc in
+                                                 v::(fitlr (merge (moveextent(e,v)) (acc) ) es)
     let fitlistr list = fitlr [] list
 
     let fitllist es = List.map (fun (x, y) -> (x + y)/2.0) (List.zip (fitlistl es) (fitlistr es))
