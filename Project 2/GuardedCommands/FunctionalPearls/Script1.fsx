17a97094ea18fb2b8b0eaf05cffe4eed952c6f97
Mathias Gammelmark <mathiasgam@hotmail.com>
2017-01-16 16:41:29 +0100

    trim character length

--- Project 2/GuardedCommands/FunctionalPearls/Script1.fsx
+++ Project 2/GuardedCommands/FunctionalPearls/Script1.fsx
@@ -15,10 +15,13 @@ open PearlUtil
 
 System.IO.Directory.SetCurrentDirectory (__SOURCE_DIRECTORY__ + @"\GCfiles");;
        
-#time
-["Ex1"; "Ex2";"Ex3"; "Ex4"; "Ex5"; "Ex6"; "Skip";
- "Ex7"; "fact"; "factRec"; "factCBV";
- "A0"; "A1"; "A2"; "A3";"A4"; "Swap"; "QuickSortV1"]
-        |> List.map (fun f -> (f+".gc",f+".ps"))
-        |> List.iter (producePS 40 40)
-#time
+//#time
+//["Ex1"; "Ex2";"Ex3"; "Ex4"; "Ex5"; "Ex6"; "Skip";
+// "Ex7"; "fact"; "factRec"; "factCBV";
+// "A0"; "A1"; "A2"; "A3";"A4"; "Swap"; "QuickSortV1"]
+//        |> List.map (fun f -> (f+".gc",f+".ps"))
+//        |> List.iter (producePS 40 40)
+//#time
+
+#load "Trees.fs"
+producePS 40 40 ("A0LongName.gc","A0LongName.ps");;
